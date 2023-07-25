bff46bbc983ef5f992a2e17d4aeeff4ddeb66c38
@Ash9311 <ashutoshmulki7@gmail.com>
2023-07-25 22:56:08 +0530

    sign in/up setup

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
 const app = express();
 const port = 3000; // Choose the desired port number
+const secretKey = '';
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
